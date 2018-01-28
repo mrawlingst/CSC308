21b7592d45cb974b636814304afe79c953a789c4
Michael Rawlings <mrawlingst@gmail.com>
2018-01-28 15:09:08 -0500

    [A01] Complete assignment 01

---
+++ Assignment01/Assignment01.playground/Contents.swift
@@ -0,0 +1,42 @@
+// Michael Rawlings
+// CSC 308 - Assignment 01
+
+import Cocoa
+
+// Find the lowest priced product in dictionary and return the info (name and price of the product)
+func lowestPrice(products: [String: (String, Double)]) -> (String, Double)
+{
+    let lowestProduct = products.min{a, b in a.value.1 < b.value.1}
+    
+    return (lowestProduct!.key, lowestProduct!.value.1)
+}
+
+// Find the highest priced product in dictionary and return the info (name and price of the product)
+func highestPrice(products: [String: (String, Double)]) -> (String, Double)
+{
+    let highestProduct = products.max{a, b in a.value.1 < b.value.1}
+    
+    return (highestProduct!.key, highestProduct!.value.1)
+}
+
+// Calls a referenced function and returns its result
+func price(products: [String: (String, Double)], funcName: ([String : (String, Double)]) -> (String, Double)) -> (String, Double)
+{
+    return funcName(products)
+}
+
+// Products to test with
+var products = [String: (String, Double)]()
+products["Strawberry Icecream"] = ("Dessert", 6.99)
+products["Orange"] = ("Fruit", 3.99)
+products["Grapes"] = ("Fruit", 2.99)
+products["Potato"] = ("Vegetable", 2.99)
+products["Onion"] = ("Vegetable", 2.55)
+products["Vanilla Icecream"] = ("Dessert", 5.99)
+
+// Highest price using price()
+print(price(products: products, funcName: highestPrice))
+
+// Lowest price using price()
+print(price(products: products, funcName: lowestPrice))
+
